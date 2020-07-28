1d68a8619c7170fadd38d420df1113be87429f0a
ltn290999 <53668823+ltn290999@users.noreply.github.com>
2020-07-28 20:43:38 +0700

    z

---
+++ ProjectWebShop/Controllers/DefaultController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectWebShop.Models;
+namespace ProjectWebShop.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            var banhangEntities = new banhangEntities();
+            var test = (from c in banhangEntities.phones where c.nhaSanXuat == "Nokia" select c).ToList();
+
+            return View(test);
+        }
+
+        // GET: Default/Details/5
+        [HttpGet]
+        public ActionResult Details(string nsx)
+        {
+            var banhangEntities = new banhangEntities();
+            nsx  = Request.Url.Segments.Last();
+            var test = (from c in banhangEntities.phones where c.nhaSanXuat == nsx select c).ToList();
+          
+            return View(test);
+        }
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
