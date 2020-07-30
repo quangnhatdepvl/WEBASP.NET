6c42cf8c2f40d4b52094a569c9ac0cab189fe06d
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-07-30 20:42:19 +0700

    z

--- New folder/WebApplication1/WebApplication1/Controllers/ShopController.cs
+++ New folder/WebApplication1/WebApplication1/Controllers/ShopController.cs
@@ -4,16 +4,139 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using PagedList;
+using PagedList.Mvc;
+
 namespace WebApplication1.Controllers
 {
     public class ShopController : Controller
     {
+        ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+ 
         // GET: Shop
         public ActionResult Index()
         {
-            ApplicationDbContext test = new ApplicationDbContext();
-           
-            return View(test.phones.ToList());
+            var sanphammoi = sanPhamMoi(6);
+            return View(sanphammoi);
         }
+        public ActionResult GiaCao()
+        {
+            var giacao = spgiacao(6);
+            return PartialView(giacao);
+
+        }
+        [HttpGet]
+        public ActionResult DanhSach()
+        {
+            List<phone> lst = new List<phone>();
+            lst = applicationDbContext.phones.ToList();
+            PagedList<phone> pg = new PagedList<phone>(lst, 1, 10);
+            return View(pg);
+        }
+        [HttpGet]
+        public ActionResult ChiTietSanPham(int id)
+        {
+            phone aphone = new phone();
+            aphone = applicationDbContext.phones.Find(id);
+
+            return View(aphone);
+        }
+        
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+        private List<phone> sanPhamMoi(int count)
+        {
+            return applicationDbContext.phones.OrderByDescending(a => a.ngaySanXuat).Take(count).ToList();
+        }
+        private List<phone> spgiacao(int count)
+        {
+            return applicationDbContext.phones.OrderByDescending(a => a.price).Take(count).ToList();
+        }
+
+        public ActionResult HangSanPham()
+        {
+            var test = (from phone in applicationDbContext.phones select phone.nhaSanXuat).Distinct();
+
+            return PartialView(test);
+        }
+        public ActionResult SanPhamTheoNhaSanXuat(string nsx)
+        {
+
+            var sanpham = new ApplicationDbContext();
+            nsx = Request.Url.Segments.Last();
+            var test = (from c in sanpham.phones where c.nhaSanXuat == nsx select c).ToList();
+
+            return View(test);
+
+
+        }
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGiohang(int gh_idPhone, string strURL)
+        {
+            List<Giohang> lstGioHang = Laygiohang();
+            Giohang sanpham = lstGioHang.Find(p => p.gh_idPhone == gh_idPhone);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(gh_idPhone);
+                lstGioHang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.gh_soLuong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoLuong()
+        {
+            int gh_TongSoLuong = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                gh_TongSoLuong = lstGiohang.Sum(n => n.gh_soLuong);
+            }
+            return gh_TongSoLuong;
+        }
+
+        private double TongTien()
+        {
+            double gh_TongTien = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                gh_TongTien = lstGiohang.Sum(n => n.gh_ThanhTien);
+
+            }
+            return gh_TongTien;
+        }
+
+        // GET: GioHang
+
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "Shop");
+            }
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+
+
+
+
     }
 }
---
+++ New folder/WebApplication1/WebApplication1/Models/Giohang.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Giohang
+    {
+        ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+
+        public int gh_idPhone { get; set; }
+        public string gh_phoneName { get; set; }
+        public string gh_typePhone { get; set; }
+        public double gh_price { get; set; }
+        public string gh_nhaSanXuat { get; set; }
+        public string gh_img_url { get; set; }
+        public Nullable<System.DateTime> gh_ngaySanXuat { get; set; }
+        public string gh_des { get; set; }
+        public int gh_luotTruyCap { get; set; }
+        public int gh_soLuong { get; set; }
+        public Double gh_ThanhTien
+        {
+            get { return gh_soLuong * gh_price; }
+        }
+        public Giohang(int maDienThoai)
+        {
+            gh_idPhone = maDienThoai;
+            phone rsPhone = applicationDbContext.phones.Single(p => p.idPhone == gh_idPhone);
+            gh_phoneName = rsPhone.phoneName;
+            gh_typePhone = rsPhone.typePhone;
+            gh_price = Double.Parse(rsPhone.price.ToString());
+            gh_nhaSanXuat = rsPhone.nhaSanXuat;
+            gh_img_url = rsPhone.img_url;
+            gh_luotTruyCap = rsPhone.luotTruyCap;
+            gh_ngaySanXuat = rsPhone.ngaySanXuat;
+            gh_soLuong = 1;
+
+        }
+
+    }
+
+}
