d70a857fbcf217a56e621f95a59f7fc8b43f3fbe
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-07-31 14:57:00 +0700

    z

--- New folder/WebApplication1/WebApplication1/App_Start/BundleConfig.cs
+++ New folder/WebApplication1/WebApplication1/App_Start/BundleConfig.cs
@@ -9,10 +9,11 @@ namespace WebApplication1
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+   
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -25,6 +26,7 @@ namespace WebApplication1
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/PagedList.css"));
         }
     }
 }
