55888e739b6113e3218f52431e3951835d0b6d1b
ltn290999 <53668823+ltn290999@users.noreply.github.com>
2020-08-31 12:17:52 +0700

    update

--- New folder/WebApplication1/WebApplication1/Global.asax.cs
+++ New folder/WebApplication1/WebApplication1/Global.asax.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = true;
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
         }
