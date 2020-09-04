c0a388c1f3f524e00516d47ebdcfef320dc0ab6d
ltn290999 <53668823+ltn290999@users.noreply.github.com>
2020-09-04 12:52:48 +0700

    Update AdminAreaRegistration.cs

--- New folder/WebApplication1/WebApplication1/Areas/Admin/AdminAreaRegistration.cs
+++ New folder/WebApplication1/WebApplication1/Areas/Admin/AdminAreaRegistration.cs
@@ -12,8 +12,17 @@ namespace WebApplication1.Areas.Admin
             }
         }
 
+
+
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+
+            context.MapRoute(
+             name: "AdminManagerBook",
+             url: "quan-ly-sach",
+             defaults: new { controller = "Admin", action = "Sach", id = UrlParameter.Optional }
+         );
+           
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
