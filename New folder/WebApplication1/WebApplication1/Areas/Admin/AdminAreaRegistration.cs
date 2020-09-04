8ae86a6312268eabacbc6dc4b1698a80589290e3
quangnhatdepvl <55266938+quangnhatdepvl@users.noreply.github.com>
2020-09-04 13:11:32 +0700

    anc

--- New folder/WebApplication1/WebApplication1/Areas/Admin/AdminAreaRegistration.cs
+++ New folder/WebApplication1/WebApplication1/Areas/Admin/AdminAreaRegistration.cs
@@ -2,11 +2,11 @@
 
 namespace WebApplication1.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public class AdminAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
@@ -14,20 +14,90 @@ namespace WebApplication1.Areas.Admin
 
 
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
 
             context.MapRoute(
              name: "AdminManagerBook",
              url: "quan-ly-sach",
              defaults: new { controller = "Admin", action = "Sach", id = UrlParameter.Optional }
+
          );
-           
             context.MapRoute(
+          name: "AdminThemCD",
+          url: "them-cd",
+          defaults: new { controller = "Admin", action = "ThemCD", id = UrlParameter.Optional }
+
+      );
+            context.MapRoute(
+            name: "AdminChiTietSach",
+            url: "chi-tiet-sach",
+            defaults: new { controller = "Admin", action = "Chitietsach", id = UrlParameter.Optional }
+
+  );
+            context.MapRoute(
+            name: "AdminDetail",
+            url: "detail",
+            defaults: new { controller = "Admin", action = "Detail", id = UrlParameter.Optional }
+
+);
+            
+
+                context.MapRoute(
+                 name: "AdminDonHang",
+                 url: "don-hang",
+                 defaults: new { controller = "Admin", action = "DonHang", id = UrlParameter.Optional }
+
+             );
+                
+
+                    context.MapRoute(
+                     name: "AdminSanPhamDaBan",
+                     url: "san-pham-da-ban",
+                     defaults: new { controller = "Admin", action = "SanPhamDaBan", id = UrlParameter.Optional }
+
+                 );
+                    {
+
+                        context.MapRoute(
+                         name: "AdminSanPhamDangGiao",
+                         url: "san-pham-dang-giao",
+                         defaults: new { controller = "Admin", action = "SanPhamDangGiao", id = UrlParameter.Optional }
+
+                     );
+                        
+
+                            context.MapRoute(
+                             name: "Suasach",
+                             url: "sua-sach",
+                             defaults: new { controller = "Admin", action = "Suasach", id = UrlParameter.Optional }
+
+                         );
+                            
+
+                                context.MapRoute(
+                                 name: "ThemMoiSach",
+                                 url: "them-moi-sach",
+                                 defaults: new { controller = "Admin", action = "ThemMoiSach", id = UrlParameter.Optional }
+
+                             );
+                                
+
+                                    context.MapRoute(
+                                     name: "ThemNXB",
+                                     url: "them-nxb",
+                                     defaults: new { controller = "Admin", action = "ThemNXNB", id = UrlParameter.Optional }
+
+                                 );
+                                    context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
-        }
-    }
-}
+                                }
+                            }
+                        }
+                    }
+                
+                
+          
